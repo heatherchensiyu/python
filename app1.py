848afcb555ec888e78296fa33328baddaf753d1e
heatherchensiyu <69836573+heatherchensiyu@users.noreply.github.com>
2020-09-29 14:14:09 +0800

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,123 @@
+print("Title of program: MCQ revision program")
+print()
+
+counter = 0
+score = 0
+total_num_of_qn = 3
+
+
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "What is the highest common factor of 8 and 12?")
+  print("   a) 10")
+  print("   b) 6")
+  print("   c) 4")
+  print("   d) 16")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong."
+    score -=1
+  elif answer == "b":
+    output = "Wrong."
+    score -=1
+  elif answer == "c":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "d":
+    output = "Wrong."
+    score -=1
+  else:
+    output = "Please choose a, b, c or d only."
+  
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+
+
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "How many atoms are there in (C3H2)4S6?")
+  print("   a) 26")
+  print("   b) 20")
+  print("   c) 16")
+  print("   d) 30")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  elif answer == "b":
+    output = "Wrong."
+    score -=1
+  elif answer == "c":
+    output = "Wrong. "
+    score -=1
+    
+  elif answer == "d":
+    output = "Wrong. "
+    score -=1
+  else:
+    output = "Please choose a, b, c or d only."
+
+  print()
+  print(output)
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+  
+
+counter +=1
+tracker = 0
+
+while tracker !=1:
+  
+  print("Q"+str(counter)+") "+ "Which represents the electronic configuration of a non-metal?")
+  print("   a) 2,1")
+  print("   b) 2,8,3")
+  print("   c) 2,8,8,2")
+  print("   d) 2,7")
+  answer = input("Your answer: ")
+  answer = answer.lower()
+  if answer == "a":
+    output = "Wrong. Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+  elif answer == "b":
+    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+  elif answer == "c":
+    output = "Wrong.  Think again - how many electron shells are filled, and which group is this in?"
+    score -=1
+    
+  elif answer == "d":
+    output = "Yes, that's right!"
+    tracker =1
+    score +=1
+  else:
+    output = "Please choose a, b, c or d only."
+
+  
+
+  print()
+  print(output.lower())
+  print()
+  print("Your current score: " + str(round((score/total_num_of_qn*100),1)) + "%"  )
+  print()
+  print()
+  
+print("End of quiz!")
+  
